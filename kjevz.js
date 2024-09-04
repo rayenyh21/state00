9f853d37e8e545beb1abd52318612bdbbd8284b5
rayenyh21 <rayenyahyaouiarab2@gmail.com>
2024-09-04 03:12:21 +0200

    Initialize project using Create React App

---
+++ kjevz.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Person: {
+        fullName: 'John Doe',
+        bio: 'Software Developer',
+        imgSrc: 'https://via.placeholder.com/150',
+        profession: 'Engineer'
+      },
+      shows: false,
+      timeInterval: 0
+    };
+    this.timer = null;
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState((prevState) => ({
+        timeInterval: prevState.timeInterval + 1
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  toggleShow = () => {
+    this.setState((prevState) => ({
+      shows: !prevState.shows
+    }));
+  }
+
+  render() {
+    const { Person, shows, timeInterval } = this.state;
+
+    return (
+      <div className="App">
+        <button onClick={this.toggleShow}>
+          {shows ? 'Hide Profile' : 'Show Profile'}
+        </button>
+        {shows && (
+          <div>
+            <h1>{Person.fullName}</h1>
+            <p>{Person.bio}</p>
+            <img src={Person.imgSrc} alt={Person.fullName} />
+            <p>Profession: {Person.profession}</p>
+          </div>
+        )}
+        <p>Time since component mounted: {timeInterval} seconds</p>
+      </div>
+    );
+  }
+}
+
